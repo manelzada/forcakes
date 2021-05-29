8dc54a8fa562dcf6c4bd2503420dea41abc5581f
manelzada <emmanoelsveiga@gmail.com>
2021-05-29 15:55:04 -0300

    Slide 1.1

--- lib/core/homepage.dart
+++ lib/core/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:forcakes/core/slide/dashboard.dart';
+import 'package:forcakes/core/slide/custom_slider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,19 +11,69 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("FORCAKES")),
+        title: Center(
+          child: Text("FORCAKES"),
+        ),
         backgroundColor: Color(0xffB39CD0),
       ),
       drawer: Drawer(),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(height: 40),
-            Dashboard(),
-          ],
-        ),
+        child: Column(children: [
+          Container(height: 50),
+          Stack(
+            alignment: AlignmentDirectional.bottomCenter,
+            children: [
+              Container(
+                height: 135,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Color(0xffB39CD0),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              Positioned(
+                left: 55,
+                bottom: -25,
+                child: CustomSlider(),
+              )
+              //Positioned(left: 50, top: -59, child: Dashboard()),
+            ],
+            clipBehavior: Clip.none,
+          ),
+        ]),
       ),
       backgroundColor: Color(0xffFBEAFF),
     );
   }
 }
+
+/* 
+CarouselSlider(
+        items: imgList,
+        options: CarouselOptions(
+            autoPlay: true,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+              });
+            }),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: imgList.map(
+          (image) {
+            int index = imgList.indexOf(image);
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index
+                      ? Color(0xffB39CD0)
+                      : Color(0xCCB39CD0)),
+            );
+          },
+        ).toList(), // this was the part the I had to add
+      )
+*/
